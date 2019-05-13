6552d7ae81bfc03f57d267a1da60cab4e5036236
David Bailey <46795205+twsdbailey@users.noreply.github.com>
2019-05-12 23:10:28 -0500

    Add files via upload

--- Pulse_train_rate_counter.ino
+++ Pulse_train_rate_counter.ino
@@ -6,19 +6,19 @@ unsigned long lasttime;
 unsigned long timesincelast = 0;
 averager cycleTime; //creates instance of the averager class where running average of "cycleTime" is stored
 
-void getTime()
+void getCycleTime()
 {
   detachInterrupt(digitalPinToInterrupt(interruptPin));//detach interrupt temporarily to complete tasks below without being interrupted
   unsigned long current = micros();
   timesincelast = current - lasttime;
   lasttime = current;
   cycleTime.idle(timesincelast); //add the elapsed cycle time to the running average
-  attachInterrupt(digitalPinToInterrupt(interruptPin), getTime, FALLING);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), getCycleTime, FALLING);
 }
 
 void setup() {
   pinMode(interruptPin, INPUT_PULLUP); //sets internal pull-up resistor for interrupt pin
-  attachInterrupt(digitalPinToInterrupt(interruptPin), getTime, FALLING); //Interrupts are called on falling edge of input
+  attachInterrupt(digitalPinToInterrupt(interruptPin), getCycleTime, FALLING); //Interrupts are called on falling edge of input
   Serial.begin(9600);
 }
 
