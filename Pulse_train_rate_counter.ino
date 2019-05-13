a704f50161cc9c457a11337b1b5e232c1912781e
David Bailey <46795205+twsdbailey@users.noreply.github.com>
2019-05-12 22:50:55 -0500

    Add files via upload

---
+++ Pulse_train_rate_counter.ino
@@ -0,0 +1,32 @@
+
+#include "averager.h";
+int scaledToHertz = 0;
+byte interruptPin = 2;// can be pin 2 or 3
+unsigned long lasttime;
+unsigned long timesincelast = 0;
+averager cycleTime; //creates instance of the averager class where running average of "cycleTime" is stored
+
+void getTime()
+{
+  detachInterrupt(digitalPinToInterrupt(interruptPin));//detach interrupt temporarily to complete tasks below without being interrupted
+  unsigned long current = micros();
+  timesincelast = current - lasttime;
+  lasttime = current;
+  cycleTime.idle(timesincelast); //add the elapsed cycle time to the running average
+  attachInterrupt(digitalPinToInterrupt(interruptPin), getTime, FALLING);
+}
+
+void setup() {
+  pinMode(interruptPin, INPUT_PULLUP); //sets internal pull-up resistor for interrupt pin
+  attachInterrupt(digitalPinToInterrupt(interruptPin), getTime, FALLING); //Interrupts are called on falling edge of input
+  Serial.begin(9600);
+}
+
+void loop() {
+  scaledToHertz = (60 * 1000000 / cycleTime()) / 60; //scaled to output cycles per second, or Hz
+  Serial.print(scaledToHertz);
+  Serial.println(" Hz");
+}
+
+
+
---
+++ averager.h
@@ -0,0 +1,47 @@
+
+/* Averager
+   created 2015
+   by A. Heidgerken-Greene, Science Museum of MN
+*/
+#include "arduino.h"
+class averager {
+public:
+
+  // variables for averaging.
+  int numSamps;
+  double * samps;
+  int pntr;
+  double tot;
+  double ave;
+
+  averager(int num = 10){// init the averaging variables.
+    pntr=tot=ave=0;
+    numSamps = num;
+    samps = new double[numSamps];
+    for(int i=0; i<numSamps; i++){
+      samps[i]=0;
+      tot+=samps[i];
+    }
+  }
+
+  double operator()(){
+    return ave;
+  }
+
+  void reset(){
+    for(int i=0; i<numSamps; i++){
+      samps[i]=0;
+    }
+    tot = ave = 0;
+  }
+
+  void idle(double newRead){
+    tot-=samps[pntr];
+    samps[pntr]=newRead;
+    tot+=newRead;
+    ave = tot/numSamps;
+    pntr=(pntr + 1)%numSamps;
+  }
+};
+
+
